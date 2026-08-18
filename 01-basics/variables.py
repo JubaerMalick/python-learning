a6e96ef5a579b475df616683679203fdddc85ce1
JubaerMalick <jubaermalick@gmail.com>
2026-08-18 18:49:28 +0600

    Refactor variables for student information

--- 01-basics/variables.py
+++ 01-basics/variables.py
@@ -1,5 +1,9 @@
-name = "Jubaer"
-age = 25
+student_name = "Jubaer"
+student_age = 25
+student_department = "Software Engineering"
+student_cgpa = 3.81
 
-print(name)
-print(age)
+print(student_name)
+print(student_age)
+print(student_department)
+print(student_cgpa)
